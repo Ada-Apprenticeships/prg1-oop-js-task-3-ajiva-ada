a8a26c2123b816d25b504cd353e861e07b4f4446
ajiva-ada <aaisha.jiva@ada.ac.uk>
2024-10-26 18:27:52 +0000

    pass T01,T02

--- oop.js
+++ oop.js
@@ -2,12 +2,18 @@ PRIORITY = { "LOW": 1, "MEDIUM": 3, "HIGH": 5, "URGENT": 7 };
 
 
 function validInteger (value) { // value can be a string or a number (integer)
-  
+  if (typeof value === 'string') {
+     const valueInteger = parseInt(value,10); //converts string to integer using base10
+     return valueInteger >= 0 && valueInteger.toString() === value; // checks if it is non-negative, checks it matches value 
+  }
+  return Number.isInteger(value) && value >= 0; //integer and non negative 
 }  
 
 
 function validatePriority(priority) { // value can be a string or a number (integer)
-  
+  const validPriorities = new Set([1,3,5,7]); //defines valid options for lookup
+  const priorityNumber= Number.isInteger(priority) ? priority : parseInt(priority, 10); //convert to num if needed
+  return validPriorities.has(priorityNumber) ? priorityNumber : 1; // 
 }
 
 
