dd3b92a76beafdd176588413c6f30b274c72cb7a
ajiva-ada <aaisha.jiva@ada.ac.uk>
2024-10-28 10:31:20 +0000

    class task

--- oop.js
+++ oop.js
@@ -18,18 +18,54 @@ function validatePriority(priority) { // value can be a string or a number (inte
 
 
 function todaysDate () {
-  
+  const now = new Date(); //get current date and time
+   
+  return [
+    now.getDate().toString().padStart(2, '0'),  // Day
+    (now.getMonth() + 1).toString().padStart(2, '0'), // Month (0-based)
+    now.getFullYear(), // Year
+    now.getHours().toString().padStart(2, '0'), // Hours
+    now.getMinutes().toString().padStart(2, '0'), // Minutes
+    now.getSeconds().toString().padStart(2, '0') // Seconds
+  ].reduce((dateTime, value, index) => {
+    if (index < 3) { // Date part
+        return dateTime + value + (index === 2 ? ' ' : '/'); // Add space after year
+    } else { // Time part
+        return dateTime + value + (index === 5 ? '' : ':'); // No colon after seconds
+    }
+  }, '');
 }
 
 
 class Task  {
+  constructor(title, priority) {
+    this._title = title; //priv
+    this.priority = priority;
+    this._added = todaysDate();
+  }
 
-  // (title, priority)
+  get title() { //getter for title, r only access
+    return this._title;
+  }
+
+  get added() { //getter for added
+    return this._added;
+  }
+
+  get priority() { //getter priority
+    return this._priority;
+  }
+
+  set priority(value) { //setter for priority  
+    this._priority = validatePriority(value);
+  }
 }
 
 
 class ToDo {
-    
+
+
+
 }
 
 
